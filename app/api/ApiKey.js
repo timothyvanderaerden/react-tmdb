16c2fcdca9ac0e355b821d42f583c23b183394e8
timothyvanderaerden <timothyvanderaerden@gmail.com>
2016-11-23 11:36:03 +0100

    Base Api

---
+++ app/api/ApiKey.js
@@ -0,0 +1,5 @@
+/**
+ * Created by timothy on 23/11/16.
+ */
+const ApiKey = 'YOUR_API_KEY';
+export default ApiKey;
---
+++ app/api/ApiUrl.js
@@ -0,0 +1,5 @@
+/**
+ * Created by timothy on 23/11/16.
+ */
+const ApiUrl = 'http://api.themoviedb.org/3/';
+export default ApiUrl;

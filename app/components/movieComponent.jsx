6025ac11f77bc20fae6938685a9eb16b002f47e3
timothyvanderaerden <timothyvanderaerden@gmail.com>
2016-11-24 19:50:17 +0100

    [Component] Added basic data to movie page

--- app/components/movieComponent.jsx
+++ app/components/movieComponent.jsx
@@ -6,10 +6,24 @@ import Store from '../store';
 import {getMovieById} from '../api/Movies';
 import {ImageUrl} from '../api/ApiUrl';
 import {Card, CardMedia, CardText, CardTitle} from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
 import CircularProgress from 'material-ui/CircularProgress';
 import {Row, Col} from 'react-flexbox-grid';
 
 export default class MovieComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.styles = {
+            chip: {
+                margin: 4,
+            },
+            wrapper: {
+                display: 'flex',
+                flexWrap: 'wrap',
+            },
+        };
+    }
+
     componentWillMount() {
         this.state = {appBarTitle: "Movie", movie: null};
         Store.dispatch({type: 'appbar_title', data: this.state.appBarTitle});
@@ -36,13 +50,53 @@ export default class MovieComponent extends React.Component {
             return (
                 <Card>
                     <CardMedia
-                        overlay={<CardTitle title={movie.original_title} />}
+                        overlay={<CardTitle title={movie.original_title}/>}
                     >
                         <img src={image}/>
                     </CardMedia>
                     <CardTitle subtitle={movie.overview}/>
                     <CardText>
-
+                        <Row>
+                            <Col xs={6}>
+                                <h2>Facts</h2>
+                                <p>
+                                    <strong style={{display: 'block'}}>Status</strong>
+                                    {movie.status}
+                                </p>
+                                <p>
+                                    <strong style={{display: 'block'}}>Original language</strong>
+                                    {movie.original_language}
+                                </p>
+                                <p>
+                                    <strong style={{display: 'block'}}>Runtime</strong>
+                                    {movie.runtime}
+                                </p>
+                                <p>
+                                    <strong style={{display: 'block'}}>Budget</strong>
+                                    $ {movie.budget}
+                                </p>
+                                <p>
+                                    <strong style={{display: 'block'}}>Revenue</strong>
+                                    $ {movie.revenue}
+                                </p>
+                                <p>
+                                    <strong style={{display: 'block'}}>Homepage</strong>
+                                    <a href={movie.homepage}>{movie.homepage}</a>
+                                </p>
+                            </Col>
+                            <Col xs={6}>
+                                <h2>Genres</h2>
+                                <div style={this.styles.wrapper}>
+                                    {movie.genres.map((genre) => {
+                                        return (
+                                            <Chip key={genre.id} style={this.styles.chip}>
+                                                {genre.name}
+                                            </Chip>
+                                        )
+                                    })}
+                                </div>
+                            </Col>
+                        </Row>
                     </CardText>
                 </Card>
             )

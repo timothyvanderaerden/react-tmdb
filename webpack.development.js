f14f52bdec1750806e53cf22dbfca969b2f241c0
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2017-02-07 21:46:33 +0100

    [CodeCleaning] use double-quotes instead of single-quotes for strings

--- webpack.development.js
+++ webpack.development.js
@@ -1,44 +1,44 @@
 const webpack = require("webpack");
 
-const nodeEnv = process.env.NODE_ENV || 'development';
+const nodeEnv = process.env.NODE_ENV || "development";
 const isProd = nodeEnv === 'production';
 
 module.exports = {
-    devtool: isProd ? 'cheap-module-source-map' : 'eval',
+    devtool: isProd ? "cheap-module-source-map" : "eval",
     entry: {
-        javascript: './entry.js',
-        html: './index.html'
+        javascript: "./entry.js",
+        html: "./index.html"
     },
     output: {
-        path: './dist',
-        filename: 'bundle.js'
+        path: "./dist",
+        filename: "bundle.js"
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 query: {
-                    presets: ['react', 'es2016']
+                    presets: ["react", "es2016"]
                 }
             },
             {
                 test: /\.css$/,
-                loader: 'style!css?modules',
+                loader: "style!css?modules",
                 include: /flexboxgrid/,
             },
             {
                 test: /\.html$/,
-                loader: 'file?name=[name].[ext]'
+                loader: "file?name=[name].[ext]"
             }
         ]
     },
     resolve: {
         extensions: [
-            '',
-            '.js',
-            '.jsx'
+            "",
+            ".js",
+            ".jsx"
         ]
     },
     devServer: {
@@ -46,7 +46,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
+            "process.env": {
                 NODE_ENV: JSON.stringify(nodeEnv)
             }
         })
